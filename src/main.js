db5fc58ff32cf24283a205a6ea376007cda9cb1c
grabolius <grabolius@gmail.com>
2025-04-07 22:58:14 +0200

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -2,4 +2,4 @@ import { Application } from '@splinetool/runtime';
 
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
-app.load('https://prod.spline.design/DmwSTLgnQxCpvJ4f/scene.splinecode');
+app.load('https://prod.spline.design/9foq1b9C42Rg-oR9/scene.splinecode');
